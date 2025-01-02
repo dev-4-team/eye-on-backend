6d004c1635d94083fb4abbd27e0bfcbd4d333452
smartcow <1995hyunwoo@naver.com>
2025-01-02 19:44:23 +0900

    feat(api): add SuccessResponseAdvice for consistent responses

---
+++ src/main/java/com/on/eye/api/config/response/SuccessResponseAdvice.java
@@ -0,0 +1,44 @@
+package com.on.eye.api.config.response;
+
+import com.on.eye.api.dto.ApiResponse;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@RestControllerAdvice(basePackages = "com.on.eye.api")
+public class SuccessResponseAdvice implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body,
+                                  MethodParameter returnType,
+                                  MediaType selectedContentType,
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType,
+                                  ServerHttpRequest request,
+                                  ServerHttpResponse response) {
+        HttpServletResponse servletResponse = ((ServletServerHttpResponse) response).getServletResponse();
+
+        int status = servletResponse.getStatus();
+        HttpStatus resolve = HttpStatus.resolve(status);
+
+        if (resolve == null)
+            return body;
+
+        if (resolve.is2xxSuccessful())
+            return ApiResponse.success(body);
+
+        // controller가 String type을 return하는 경우 error 발생 가능성 있음
+
+        return body;
+    }
+}
