89c803af6aceecc322a2ad67e20f895551aeb3fa
smartcow <1995hyunwoo@naver.com>
2025-01-22 16:04:01 +0900

    feat(constants): add constant definitions for API

---
+++ src/main/java/com/on/eye/api/constants/AuthConstants.java
@@ -0,0 +1,15 @@
+package com.on.eye.api.constants;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class AuthConstants {
+    public static final String BEARER = "Bearer ";
+    public static final String TOKEN_ROLE = "role";
+    public static final String TOKEN_TYPE = "type";
+    public static final String TOKEN_ISSUER = "eye-on";
+    public static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+    public static final String REFRESH_TOKEN = "REFRESH_TOKEN";
+    public static final String[] ALLOW_URLS = {"/login", "/auth/login/kakao/**"};
+}
