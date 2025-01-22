6eaa73965624e939c0344f0f122dce7407b4b8b5
smartcow <1995hyunwoo@naver.com>
2025-01-22 16:07:36 +0900

    feat(auth): add user, profile, oauthInfo entities

---
+++ src/main/java/com/on/eye/api/auth/model/entity/Profile.java
@@ -0,0 +1,51 @@
+package com.on.eye.api.auth.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.Embedded;
+
+import com.on.eye.api.common.model.vo.ImageVo;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Profile {
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String nickname;
+
+    @Embedded private ImageVo profileImageUrl;
+
+    public void withdraw() {
+        this.nickname = "탈퇴한 유저";
+        this.profileImageUrl = null;
+        this.email = null;
+    }
+
+    @Builder
+    public Profile(String nickname, String profileImageUrl, String email) {
+        this.nickname = nickname;
+        this.profileImageUrl = ImageVo.of(profileImageUrl);
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Profile profile = (Profile) obj;
+        return this.nickname.equals(profile.getNickname())
+                && this.profileImageUrl.getUrl().equals(profile.getProfileImageUrl().getUrl());
+    }
+
+    @Override
+    public int hashCode() {
+        return this.nickname.hashCode() + this.profileImageUrl.getUrl().hashCode();
+    }
+}
