7ec4112ce7216471d08b06009b20f304f9a667a5
smartcow <1995hyunwoo@naver.com>
2025-01-22 16:04:18 +0900

    feat(api): enhance ApiResponse with additional error details

--- src/main/java/com/on/eye/api/dto/ApiResponse.java
+++ src/main/java/com/on/eye/api/dto/ApiResponse.java
@@ -1,6 +1,10 @@
 package com.on.eye.api.dto;
 
+import java.time.LocalDateTime;
+
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.on.eye.api.exception.ErrorReason;
+
 import lombok.Getter;
 
 @Getter
@@ -9,6 +13,10 @@ public class ApiResponse<T> {
     private boolean success = true;
     private String message;
     private T data = null;
+    private LocalDateTime timestamp;
+    private int status;
+    private String code;
+    private String path;
 
     public ApiResponse(String message, boolean success) {
         this.message = message;
@@ -30,6 +38,14 @@ public class ApiResponse<T> {
         this.success = b;
     }
 
+    public ApiResponse(ErrorReason errorReason, String path) {
+        this.message = errorReason.getMessage();
+        this.status = errorReason.getStatus();
+        this.code = errorReason.getCode();
+        this.path = path;
+        this.timestamp = LocalDateTime.now();
+    }
+
     public static <T> ApiResponse<T> success(T data) {
         return new ApiResponse<>("success", data);
     }
@@ -40,5 +56,9 @@ public class ApiResponse<T> {
 
     public static <T> ApiResponse<T> error(String message, T data) {
         return new ApiResponse<>(message, data, false);
+    }
+
+    public static <T> ApiResponse<T> error(ErrorReason errorReason, String path) {
+        return new ApiResponse<>(errorReason, path);
     }
 }
