4cd5f003cfc094bd31beb47d08fbb6d03ba29238
smartcow <1995hyunwoo@naver.com>
2025-02-03 13:31:03 +0900

    feat(repository): add custom queries for location distance and protest location

--- src/main/java/com/on/eye/api/repository/LocationRepository.java
+++ src/main/java/com/on/eye/api/repository/LocationRepository.java
@@ -1,11 +1,49 @@
 package com.on.eye.api.repository;
 
+import java.math.BigDecimal;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.on.eye.api.domain.Location;
+import com.on.eye.api.dto.ProtestLocationDto;
 
 public interface LocationRepository extends JpaRepository<Location, Long> {
     Optional<Location> findByName(String name);
+
+    @Query(
+            value =
+                    """
+                                 SELECT earth_distance(
+                                     ll_to_earth(:userLat, :userLng),
+                                     ll_to_earth(l.latitude, l.longitude)
+                                     )
+                                 FROM locations l
+                                 WHERE l.id = :locationId
+                            """,
+            nativeQuery = true)
+    Double calculateDistance(
+            @Param("userLat") BigDecimal userLat,
+            @Param("userLng") BigDecimal userLng,
+            @Param("locationId") Long locationId);
+
+    @Query(
+            """
+                    select new com.on.eye.api.dto.ProtestLocationDto(
+                        p.id,
+                        p.radius,
+                        l.id,
+                        l.latitude,
+                        l.longitude
+                    )
+                    from Protest p
+                    join p.locationMappings lm
+                    join lm.location l
+                    where p.id = :protestId
+                    order by lm.sequence
+                    limit 1
+            """)
+    Optional<ProtestLocationDto> findFirstLocationByProtestId(@Param("protestId") Long protestId);
 }
