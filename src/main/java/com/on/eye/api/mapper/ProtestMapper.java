88a667cd0c4f423ca5879351b2c9e976a3818195
smartcow <1995hyunwoo@naver.com>
2025-02-03 13:30:19 +0900

    feat(mapper): add radius calculation to ProtestMapper

--- src/main/java/com/on/eye/api/mapper/ProtestMapper.java
+++ src/main/java/com/on/eye/api/mapper/ProtestMapper.java
@@ -1,15 +1,16 @@
 package com.on.eye.api.mapper;
 
-import java.util.List;
-
 import com.on.eye.api.domain.Protest;
 import com.on.eye.api.dto.ProtestCreateDto;
 import com.on.eye.api.dto.ProtestCreateMapping;
 import com.on.eye.api.dto.ProtestDetailDto;
 
+import java.util.List;
+
 public class ProtestMapper {
 
-    private ProtestMapper() {}
+    private ProtestMapper() {
+    }
 
     public static List<ProtestCreateMapping> toEntity(List<ProtestCreateDto> protestCreateDtos) {
         return protestCreateDtos.stream()
@@ -25,10 +26,23 @@ public class ProtestMapper {
                                             .organizer(protestCreateDto.getOrganizer())
                                             .declaredParticipants(
                                                     protestCreateDto.getDeclaredParticipants())
+                                            .radius(calRadius(protestCreateDto.getDeclaredParticipants()))
                                             .build();
                             return new ProtestCreateMapping(protestCreateDto, protest);
                         })
                 .toList();
+    }
+
+    private static Integer calRadius(int declaredParticipants) {
+        int minRadius = 200;
+        int maxRadius = 500;
+        int minParticipants = 100;
+
+        Double logParticipants = Math.log10(declaredParticipants) - Math.log10(minParticipants);
+        Double logRange = Math.log10(maxRadius) - Math.log10(minParticipants);
+        double normalizedLogParticipants = logParticipants / logRange;
+        double radius = minRadius + (maxRadius - minRadius) * normalizedLogParticipants;
+        return (int) Math.round(radius);
     }
 
     public static ProtestDetailDto toDto(Protest protest) {
