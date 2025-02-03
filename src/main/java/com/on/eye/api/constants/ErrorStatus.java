f54e3cd7e950ff0803c0df7d91c1151cac912eb3
smartcow <1995hyunwoo@naver.com>
2025-02-03 14:00:29 +0900

    feat(exception): add new error codes for protest-related cases

--- src/main/java/com/on/eye/api/constants/ErrorStatus.java
+++ src/main/java/com/on/eye/api/constants/ErrorStatus.java
@@ -9,5 +9,7 @@ public class ErrorStatus {
     public static final int UNAUTHORIZED = 401;
     public static final int FORBIDDEN = 403;
     public static final int NOT_FOUND = 404;
+    public static final int CONFLICT = 409;
+    public static final int UNPROCESSABLE_ENTITY = 422;
     public static final int INTERNAL_SERVER = 500;
 }
