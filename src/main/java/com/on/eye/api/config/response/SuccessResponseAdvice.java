48aa35fbbb58873a6b0128d033802e2087d290fd
smartcow <1995hyunwoo@naver.com>
2025-02-04 13:44:59 +0900

    refactor(api): replace ApiResponse with SuccessResponse

--- src/main/java/com/on/eye/api/config/response/SuccessResponseAdvice.java
+++ src/main/java/com/on/eye/api/config/response/SuccessResponseAdvice.java
@@ -9,27 +9,29 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpResponse;
+import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
-import com.on.eye.api.dto.ApiResponse;
+import com.on.eye.api.dto.SuccessResponse;
 
 @RestControllerAdvice(basePackages = "com.on.eye.api")
 public class SuccessResponseAdvice implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(
-            MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+            @NonNull MethodParameter returnType,
+            @NonNull Class<? extends HttpMessageConverter<?>> converterType) {
         return true;
     }
 
     @Override
     public Object beforeBodyWrite(
             Object body,
-            MethodParameter returnType,
-            MediaType selectedContentType,
-            Class<? extends HttpMessageConverter<?>> selectedConverterType,
-            ServerHttpRequest request,
-            ServerHttpResponse response) {
+            @NonNull MethodParameter returnType,
+            @NonNull MediaType selectedContentType,
+            @NonNull Class<? extends HttpMessageConverter<?>> selectedConverterType,
+            @NonNull ServerHttpRequest request,
+            @NonNull ServerHttpResponse response) {
         HttpServletResponse servletResponse =
                 ((ServletServerHttpResponse) response).getServletResponse();
 
@@ -38,7 +40,7 @@ public class SuccessResponseAdvice implements ResponseBodyAdvice<Object> {
 
         if (resolve == null) return body;
 
-        if (resolve.is2xxSuccessful()) return ApiResponse.success(body);
+        if (resolve.is2xxSuccessful()) return new SuccessResponse<>(status, body);
 
         // controller가 String type을 return하는 경우 error 발생 가능성 있음
 
