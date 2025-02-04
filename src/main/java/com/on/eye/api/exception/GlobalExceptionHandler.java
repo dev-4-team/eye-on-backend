1c3c3194f5d46926102e22905412592aa07c5aee
smartcow <1995hyunwoo@naver.com>
2025-02-04 13:55:14 +0900

    refactor(exception): improve global exception handling
    Extended `GlobalExceptionHandler` to inherit from `ResponseEntityExceptionHandler` for better structure. Standardized error responses with `ErrorResponse` and added detailed error handling for validation and custom exceptions.

--- src/main/java/com/on/eye/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/on/eye/api/exception/GlobalExceptionHandler.java
@@ -1,54 +1,129 @@
 package com.on.eye.api.exception;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
+
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.on.eye.api.dto.ApiResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.on.eye.api.dto.ErrorResponse;
 
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 @RestControllerAdvice
 @Slf4j
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<Void>> handleException(Exception e) {
-        log.error("Unhandled exception occurred: ", e);
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ApiResponse.error(e.getMessage()));
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex,
+            Object body,
+            @NonNull HttpHeaders headers,
+            HttpStatusCode statusCode,
+            @NonNull WebRequest request) {
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+        String url =
+                ServletUriComponentsBuilder.fromRequest(servletWebRequest.getRequest())
+                        .build()
+                        .toUriString();
+
+        ErrorResponse errorResponse =
+                new ErrorResponse(statusCode.value(), statusCode.toString(), ex.getMessage(), url);
+
+        return super.handleExceptionInternal(ex, errorResponse, headers, statusCode, request);
     }
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ApiResponse<Void>> handleResourceNotFoundException(
-            ResourceNotFoundException e) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiResponse.error(e.getMessage()));
+    @Override
+    @SneakyThrows
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            @NonNull HttpHeaders headers,
+            HttpStatusCode status,
+            @NonNull WebRequest request) {
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        ServletWebRequest servletWebRequest = (ServletWebRequest) request;
+        String url =
+                ServletUriComponentsBuilder.fromRequest(servletWebRequest.getRequest())
+                        .build()
+                        .toUriString();
+        Map<String, Object> fieldAndErrorMessages =
+                fieldErrors.stream()
+                        .collect(
+                                Collectors.toMap(
+                                        FieldError::getField,
+                                        error ->
+                                                error.getDefaultMessage() != null
+                                                        ? error.getDefaultMessage()
+                                                        : "No message available",
+                                        (existing, replacement) -> existing));
+
+        String errorsToJsonString = objectMapper.writeValueAsString(fieldAndErrorMessages);
+        ErrorResponse errorResponse =
+                new ErrorResponse(status.value(), status.toString(), errorsToJsonString, url);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Map<String, String>>> handleValidationException(
-            MethodArgumentNotValidException e) {
-        log.error("Validation exception occurred: ", e);
+    @ExceptionHandler(CustomCodeException.class)
+    public ResponseEntity<ErrorResponse> handleCustomCodeException(
+            CustomCodeException e, HttpServletRequest request) {
+        BaseErrorCode errorCode = e.getErrorCode();
+        ErrorReason errorReason = errorCode.getErrorReason();
+        return ResponseEntity.status(HttpStatus.valueOf(errorReason.getStatus()))
+                .body(new ErrorResponse(errorReason, request.getRequestURL().toString()));
+    }
 
-        Map<String, String> errors = new HashMap<>();
-        e.getBindingResult()
-                .getAllErrors()
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorResponse> handleConstraintViolationException(
+            ConstraintViolationException e, HttpServletRequest request) {
+        Map<String, Object> bindingErrors = new HashMap<>();
+
+        e.getConstraintViolations()
                 .forEach(
-                        error -> {
-                            if (error instanceof FieldError fieldError) {
-                                errors.put(fieldError.getField(), fieldError.getDefaultMessage());
-                            } else {
-                                errors.put(error.getObjectName(), error.getDefaultMessage());
-                            }
+                        constraintViolation -> {
+                            List<String> propertyPath =
+                                    List.of(
+                                            constraintViolation
+                                                    .getPropertyPath()
+                                                    .toString()
+                                                    .split("\\."));
+                            String path =
+                                    propertyPath.stream()
+                                            .skip(propertyPath.size() - 1L)
+                                            .findFirst()
+                                            .orElse(null);
+                            bindingErrors.put(path, constraintViolation.getMessage());
                         });
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(ApiResponse.error("Invalid data", errors));
+        ErrorReason errorReason =
+                ErrorReason.builder()
+                        .code("BAD_REQUEST")
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .message(bindingErrors.toString())
+                        .build();
+        ErrorResponse errorResponse =
+                new ErrorResponse(errorReason, request.getRequestURL().toString());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
