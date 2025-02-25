2e2060191e195fe1b9ae2690ed4f4d7fa9c2f522
smartcow <1995hyunwoo@naver.com>
2025-02-25 11:39:50 +0900

    feat(repository): add method to fetch verifications by protest ID

--- src/main/java/com/on/eye/api/repository/ParticipantVerificationRepository.java
+++ src/main/java/com/on/eye/api/repository/ParticipantVerificationRepository.java
@@ -1,8 +1,12 @@
 package com.on.eye.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.on.eye.api.domain.ParticipantsVerification;
 
 public interface ParticipantVerificationRepository
-        extends JpaRepository<ParticipantsVerification, Long> {}
+        extends JpaRepository<ParticipantsVerification, Long> {
+    List<ParticipantsVerification> getParticipantsVerificationByProtest_Id(Long protestId);
+}
