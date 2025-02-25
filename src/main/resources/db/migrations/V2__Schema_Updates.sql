7e654c5b30d65b3203856fd6ef00b0c2d8be7d2f
smartcow <1995hyunwoo@naver.com>
2025-02-25 14:55:22 +0900

    fix(migrations): remove redundant constraints

--- src/main/resources/db/migrations/V2__Schema_Updates.sql
+++ src/main/resources/db/migrations/V2__Schema_Updates.sql
@@ -1,9 +1,6 @@
 ALTER TABLE participants_verifications
     ADD CONSTRAINT uc_participants_verifications_protest UNIQUE (protest_id);
 
-ALTER TABLE protest_location_mappings
-    ADD CONSTRAINT unique_protest_location_id UNIQUE (protest_id, location_id);
-
 ALTER TABLE organizers
     ADD title VARCHAR(255);
 
