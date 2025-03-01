077a363122908dd951b3138cdbe305d6859a9702
smartcow <1995hyunwoo@naver.com>
2025-03-01 12:57:50 +0900

    feat(api): log active profiles on application startup

--- src/main/java/com/on/eye/api/ApiApplication.java
+++ src/main/java/com/on/eye/api/ApiApplication.java
@@ -1,16 +1,34 @@
 package com.on.eye.api;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.ApplicationListener;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan(basePackages = "com.on.eye.api.common.properties")
 @EnableJpaAuditing
-public class ApiApplication {
+@RequiredArgsConstructor
+@Slf4j
+public class ApiApplication implements ApplicationListener<ApplicationReadyEvent> {
+
+    private final Environment environment;
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Override
+    public void onApplicationEvent(@NonNull ApplicationReadyEvent event) {
+        log.info("applicationReady status" + Arrays.toString(environment.getActiveProfiles()));
     }
 }
