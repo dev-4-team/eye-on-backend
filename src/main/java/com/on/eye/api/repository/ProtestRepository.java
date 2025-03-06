ec556603bcf0f2bbf65123922bfa19812ec9dc21
smartcow <1995hyunwoo@naver.com>
2025-03-06 21:36:52 +0900

    refactor(repository): remove unused protest query methods

--- src/main/java/com/on/eye/api/repository/ProtestRepository.java
+++ src/main/java/com/on/eye/api/repository/ProtestRepository.java
@@ -9,15 +9,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.on.eye.api.domain.Protest;
-import com.on.eye.api.domain.ProtestStatus;
 
 public interface ProtestRepository extends JpaRepository<Protest, Long> {
-    // 상태별 시위 목록 조회
-    List<Protest> findByStatus(ProtestStatus status);
-
-    List<Protest> findByStartDateTimeBetween(
-            LocalDateTime startDateTimeAfter, LocalDateTime startDateTimeBefore);
-
     List<Protest> findByStartDateTimeAfter(LocalDateTime startDateTime);
 
     @Query("SELECT p FROM Protest p LEFT JOIN FETCH p.organizer WHERE p.id = :protestId")
