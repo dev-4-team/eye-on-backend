200445d0bdc19f5b4fad96085ed5f51110020ccc
smartcow <1995hyunwoo@naver.com>
2025-03-22 12:35:24 +0900

    feat(cheer): add ProtestCheerCount entity and migration

---
+++ src/main/java/com/on/eye/api/cheer/entity/ProtestCheerCount.java
@@ -0,0 +1,37 @@
+package com.on.eye.api.cheer.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import com.on.eye.api.cheer.dto.CheerStat;
+import com.on.eye.api.global.common.model.entity.BaseTimeEntity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ProtestCheerCount extends BaseTimeEntity {
+    @Id private Long protestId;
+
+    @Column(nullable = false)
+    private Integer cheerCount;
+
+    @Builder
+    public ProtestCheerCount(Long protestId, Integer cheerCount) {
+        this.protestId = protestId;
+        this.cheerCount = cheerCount;
+    }
+
+    public static ProtestCheerCount from(Long protestId) {
+        return new ProtestCheerCount(protestId, 0);
+    }
+
+    public static ProtestCheerCount from(CheerStat cheerStat) {
+        return new ProtestCheerCount(cheerStat.protestId(), cheerStat.cheerCount());
+    }
+}
