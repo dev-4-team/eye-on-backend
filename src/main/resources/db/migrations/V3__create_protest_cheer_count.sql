8c1c9937a08d147c457a93aa16e97061af9f7172
smartcow <1995hyunwoo@naver.com>
2025-03-22 12:35:24 +0900

    feat(cheer): add ProtestCheerCount entity and migration

---
+++ src/main/resources/db/migrations/V3__create_protest_cheer_count.sql
@@ -0,0 +1,8 @@
+CREATE TABLE protest_cheer_count
+(
+    protest_id  BIGINT NOT NULL,
+    created_at  TIMESTAMP WITH TIME ZONE,
+    updated_at  TIMESTAMP WITH TIME ZONE,
+    cheer_count INT    NOT NULL,
+    CONSTRAINT pk_protestCheerCount PRIMARY KEY (protest_id)
+);
