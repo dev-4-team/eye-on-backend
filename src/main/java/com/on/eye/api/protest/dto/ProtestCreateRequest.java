48c463ad3f3b4532c89df2972421158449d92194
smartcow <1995hyunwoo@naver.com>
2025-03-22 13:00:26 +0900

    refactor(validation): update protest duration logic and error codes.

--- src/main/java/com/on/eye/api/protest/dto/ProtestCreateRequest.java
+++ src/main/java/com/on/eye/api/protest/dto/ProtestCreateRequest.java
@@ -1,15 +1,17 @@
 package com.on.eye.api.protest.dto;
 
-import com.on.eye.api.location.dto.LocationDto;
-import com.on.eye.api.protest.validator.ValidProtestDateTimeRange;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.Builder;
 
-import java.time.LocalDateTime;
-import java.util.List;
+import com.on.eye.api.location.dto.LocationDto;
+import com.on.eye.api.protest.validator.ValidProtestDateTimeRange;
+
+import lombok.Builder;
 
 @ValidProtestDateTimeRange
 @Builder
@@ -17,9 +19,7 @@ public record ProtestCreateRequest(
         String title,
         @NotNull(message = "Start date and time cannot be null") LocalDateTime startDateTime,
         @NotNull(message = "End date and time cannot be null") LocalDateTime endDateTime,
-        @NotNull(message = "organizer name cannot be null")
-        String organizer,
+        @NotNull(message = "organizer name cannot be null") String organizer,
         @NotNull(message = "declaredParticipants cannot be null") @Min(0) @Max(5000000)
-        Integer declaredParticipants,
-        @NotEmpty(message = "locations cannot be empty") List<LocationDto> locations) {
-}
+                Integer declaredParticipants,
+        @NotEmpty(message = "locations cannot be empty") List<LocationDto> locations) {}
