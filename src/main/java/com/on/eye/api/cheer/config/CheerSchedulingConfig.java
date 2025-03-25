7faff64d6591812d16673a66331f019373610747
smartcow <1995hyunwoo@naver.com>
2025-03-25 20:59:22 +0900

    chore(style): apply code formatter

--- src/main/java/com/on/eye/api/cheer/config/CheerSchedulingConfig.java
+++ src/main/java/com/on/eye/api/cheer/config/CheerSchedulingConfig.java
@@ -3,9 +3,7 @@ package com.on.eye.api.cheer.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-/**
- * 스케줄링 설정 클래스 주기적 작업 실행을 위한 설정을 포함.
- */
+/** 스케줄링 설정 클래스 주기적 작업 실행을 위한 설정을 포함. */
 @Configuration
 @EnableScheduling
 public class CheerSchedulingConfig {
--- src/main/java/com/on/eye/api/global/constants/Constants.java
+++ src/main/java/com/on/eye/api/global/constants/Constants.java
@@ -8,12 +8,12 @@ public final class Constants {
     public static final int MILLI_TO_SECOND = 1000;
     public static final String ASSET_DOMAIN = "https://asset.eye-on.kr";
     public static final String[] SWAGGER_PATTERNS = {
-            "/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs",
+        "/swagger-resources/**", "/swagger-ui/**", "/v3/api-docs/**", "/v3/api-docs",
     };
     public static final String[] CORS_ALLOW_LIST = {
-            "http://localhost:3000",
-            "http://localhost:63342",
-            "https://www.eye-on.kr",
-            "https://eye-on.kr"
+        "http://localhost:3000",
+        "http://localhost:63342",
+        "https://www.eye-on.kr",
+        "https://eye-on.kr"
     };
 }
