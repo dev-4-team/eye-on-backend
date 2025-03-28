5152eb247f93aaff2ec337bc335a867ec346324c
smartcow <1995hyunwoo@naver.com>
2025-03-28 10:25:38 +0900

    refactor(protest): entity domain pattern 적용 > `create` method > services & repositories(Protest, Location, Organizer)

--- src/main/java/com/on/eye/api/location/service/LocationService.java
+++ src/main/java/com/on/eye/api/location/service/LocationService.java
@@ -1,35 +1,45 @@
 package com.on.eye.api.location.service;
 
-import org.springframework.stereotype.Service;
-
 import com.on.eye.api.location.dto.LocationDto;
 import com.on.eye.api.location.dto.ProtestLocationDto;
 import com.on.eye.api.location.entity.Location;
+import com.on.eye.api.location.entity.Locations;
+import com.on.eye.api.location.entity.ProtestLocationMappings;
 import com.on.eye.api.location.error.exception.LocationNotFoundException;
 import com.on.eye.api.location.repository.LocationRepository;
 import com.on.eye.api.protest.entity.Protest;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class LocationService {
     private final LocationRepository locationRepository;
+    private static final double SIMILARITY_THRESHOLD = 0.35;
 
-    public Location getOrCreateLocation(LocationDto locationDto) {
-        double similarity = 0.35;
+    public ProtestLocationMappings assignLocationMappings(Protest protest, List<LocationDto> locationDtos) {
+        Locations locations = getLocations(locationDtos);
+        return new ProtestLocationMappings(protest, locations);
+    }
+
+    private Locations getLocations(List<LocationDto> locationDtos) {
+        Locations locations = new Locations();
+        for (LocationDto locationDto : locationDtos) {
+            Location location = getOrCreateLocation(locationDto);
+            locations.add(location);
+        }
+
+        return locations;
+    }
+
+    private Location getOrCreateLocation(LocationDto locationDto) {
         return locationRepository
-                .findMostSimilarLocation(locationDto.name(), similarity)
-                .orElseGet(
-                        () ->
-                                locationRepository.save(
-                                        Location.builder()
-                                                .name(locationDto.name())
-                                                .latitude(locationDto.latitude())
-                                                .longitude(locationDto.longitude())
-                                                .build()));
+                .findMostSimilarLocation(locationDto.name(), SIMILARITY_THRESHOLD)
+                .orElseGet(() -> locationRepository.save(Location.from(locationDto)));
     }
 
     public ProtestLocationDto getProtestCenterLocation(Protest protest) {
