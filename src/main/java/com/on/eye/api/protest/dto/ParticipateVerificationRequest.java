fda3a75e9120f9d4446541e0844ab48ad37500d4
smartcow <1995hyunwoo@naver.com>
2025-03-29 10:22:09 +0900

    refactor(protest): replace `ParticipateVerificationRequest` with `Coordinate`

--- src/main/java/com/on/eye/api/protest/dto/ParticipateVerificationRequest.java
+++
@@ -1,8 +0,0 @@
-package com.on.eye.api.protest.dto;
-
-import java.math.BigDecimal;
-
-import jakarta.validation.constraints.NotNull;
-
-public record ParticipateVerificationRequest(
-        @NotNull BigDecimal longitude, @NotNull BigDecimal latitude) {}
