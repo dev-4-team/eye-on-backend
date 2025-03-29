4b4c68527b81eddc77bab97372c8a2461172d637
yujin0124 <yujin0124@khu.ac.kr>
2025-03-29 16:12:00 +0900

    feat(cheer): Add CheerService interface and CheerDbService implementation

---
+++ src/main/java/com/on/eye/api/cheer/service/CheerService.java
@@ -0,0 +1,30 @@
+package com.on.eye.api.cheer.service;
+
+import com.on.eye.api.cheer.dto.CheerStat;
+
+import java.util.List;
+
+public interface CheerService {
+    /**
+     * 시위에 대한 응원 수를 증가시킴
+     *
+     * @param protestId 시위 ID
+     * @return 증가 후의 응원 수
+     */
+    Integer cheerProtest(Long protestId);
+
+    /**
+     * 시위의 현재 응원 통계를 조회
+     *
+     * @param protestId 시위 ID
+     * @return 응원 통계 정보
+     */
+    CheerStat getCheerStat(Long protestId);
+
+    /**
+     * 모든 시위의 응원 통계를 조회
+     *
+     * @return 모든 시위의 응원 통계 목록
+     */
+    List<CheerStat> getAllCheerStats();
+}
