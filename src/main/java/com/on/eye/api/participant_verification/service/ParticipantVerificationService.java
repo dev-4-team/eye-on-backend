8f8ece9f1fb2fc32055f04bee6b9f97889cb7a68
smartcow <1995hyunwoo@naver.com>
2025-03-31 16:58:11 +0900

    refactor(service): reorganize methods for clarity and modularity

--- src/main/java/com/on/eye/api/participant_verification/service/ParticipantVerificationService.java
+++ src/main/java/com/on/eye/api/participant_verification/service/ParticipantVerificationService.java
@@ -1,11 +1,5 @@
 package com.on.eye.api.participant_verification.service;
 
-import java.util.Optional;
-
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import com.on.eye.api.global.common.util.LocalDateTimeUtils;
 import com.on.eye.api.global.config.security.AnonymousIdGenerator;
 import com.on.eye.api.global.config.security.SecurityUtils;
@@ -15,8 +9,12 @@ import com.on.eye.api.participant_verification.repository.ParticipantVerificatio
 import com.on.eye.api.protest.dto.Coordinate;
 import com.on.eye.api.protest.entity.Protest;
 import com.on.eye.api.protest.error.exception.DuplicateVerificationException;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -30,19 +28,27 @@ public class ParticipantVerificationService {
         String anonymousUserId = anonymousIdGenerator.generateAnonymousUserId(userId);
 
         // 가장 최근 인증 기록과, 현재 위치에 대한 비정상 이동 패턴 검출
+        checkAbnormalMovementPattern(anonymousUserId, userCoordinate);
+
+        saveVerification(protest, anonymousUserId);
+    }
+
+    private void checkAbnormalMovementPattern(String anonymousUserId, Coordinate userCoordinate) {
         Optional<VerificationHistory> mostRecentVerification =
                 participantVerificationRepository.findMostRecentVerification(
                         LocalDateTimeUtils.todayStartTime(), anonymousUserId);
+
         mostRecentVerification.ifPresent(
                 verificationHistory ->
                         verificationHistory.detectAbnormalMovementPattern(userCoordinate));
+    }
 
+    private void saveVerification(Protest protest, String anonymousUserId) {
         ParticipantsVerification verification =
                 new ParticipantsVerification(protest, anonymousUserId);
         try {
             participantVerificationRepository.save(verification);
         } catch (DataIntegrityViolationException e) {
-            // 중복 인증 검증
             throw DuplicateVerificationException.EXCEPTION;
         }
     }
