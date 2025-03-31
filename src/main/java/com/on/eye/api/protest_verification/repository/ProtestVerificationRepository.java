22eefcee20451b1598989920b2d32d5e48f92fed
smartcow <1995hyunwoo@naver.com>
2025-03-31 12:45:45 +0900

    refactor(protest_verification): 인증수 조회 method를 protestService에서 분리

--- src/main/java/com/on/eye/api/protest_verification/repository/ProtestVerificationRepository.java
+++ src/main/java/com/on/eye/api/protest_verification/repository/ProtestVerificationRepository.java
@@ -1,9 +1,13 @@
 package com.on.eye.api.protest_verification.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import com.on.eye.api.protest_verification.dto.ProtestVerificationResponse;
 import com.on.eye.api.protest_verification.entity.ProtestVerification;
 
 public interface ProtestVerificationRepository extends JpaRepository<ProtestVerification, Long> {
@@ -13,4 +17,13 @@ public interface ProtestVerificationRepository extends JpaRepository<ProtestVeri
     void increaseVerifiedNum(Long protestId);
 
     ProtestVerification findByProtestId(Long protestId);
+
+    @Query(
+            """
+                    SELECT new com.on.eye.api.protest_verification.dto.ProtestVerificationResponse(p.id, pv.verifiedNum) FROM ProtestVerification pv
+                        LEFT JOIN pv.protest p
+                            WHERE p.startDateTime >= :startDateTime and p.endDateTime < :endDateTime
+                    """)
+    List<ProtestVerificationResponse> findAllByProtestDateTimeBetween(
+            LocalDateTime startDateTime, LocalDateTime endDateTime);
 }
