e2a44143edcdda7d10ca8fca5d27b58b028fc434
smartcow <1995hyunwoo@naver.com>
2025-03-31 12:45:45 +0900

    refactor(protest_verification): 인증수 조회 method를 protestService에서 분리

--- src/main/java/com/on/eye/api/protest_verification/service/ProtestVerificationService.java
+++ src/main/java/com/on/eye/api/protest_verification/service/ProtestVerificationService.java
@@ -1,9 +1,16 @@
 package com.on.eye.api.protest_verification.service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.on.eye.api.global.common.util.LocalDateTimeUtils;
 import com.on.eye.api.protest.entity.Protest;
+import com.on.eye.api.protest_verification.dto.ProtestVerificationResponse;
+import com.on.eye.api.protest_verification.entity.ProtestVerification;
 import com.on.eye.api.protest_verification.repository.ProtestVerificationRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -16,5 +23,21 @@ public class ProtestVerificationService {
     @Transactional
     public void updateProtestVerification(Protest protest) {
         protestVerificationRepository.increaseVerifiedNum(protest.getId());
+    }
+
+    @Transactional(readOnly = true)
+    public List<ProtestVerificationResponse> getTodayProtestVerifications(
+            Long protestId, LocalDate date) {
+        LocalDateTime todayStartTime = LocalDateTimeUtils.getStartOfDay(date.atStartOfDay());
+        LocalDateTime todayEndTime = LocalDateTimeUtils.getEndOfDay(date.atStartOfDay());
+        if (isRequestAll(protestId))
+            return protestVerificationRepository.findAllByProtestDateTimeBetween(
+                    todayStartTime, todayEndTime);
+        ProtestVerification verification = protestVerificationRepository.findByProtestId(protestId);
+        return List.of(ProtestVerificationResponse.from(verification));
+    }
+
+    private boolean isRequestAll(Long protestId) {
+        return protestId == null;
     }
 }
