8ab65743f63b04873830683ce8d70186288b4b09
smartcow <1995hyunwoo@naver.com>
2025-04-12 15:31:03 +0900

    hot-fix(cheer): replace cheerDb with cheerCache

--- src/main/java/com/on/eye/api/cheer/controller/CheerRestController.java
+++ src/main/java/com/on/eye/api/cheer/controller/CheerRestController.java
@@ -1,15 +1,13 @@
 package com.on.eye.api.cheer.controller;
 
-import java.util.List;
-
+import com.on.eye.api.cheer.dto.CheerStat;
+import com.on.eye.api.cheer.service.CheerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.on.eye.api.cheer.dto.CheerStat;
-import com.on.eye.api.cheer.service.CheerService;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
 
 /**
  * 응원 관련 REST API 컨트롤러 HTTP 요청을 통한 응원 기능을 제공. Polling vs WebSocket 비교 or WebSocket 불가능한 상황 대처 위해 생성
@@ -19,7 +17,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class CheerRestController {
-    private final CheerService cheerDbService;
+    private final CheerService cheerCacheService;
 
     /**
      * 특정 시위의 응원 수를 조회
@@ -29,7 +27,7 @@ public class CheerRestController {
      */
     @GetMapping("/{protestId}")
     public ResponseEntity<CheerStat> getCheerStat(@PathVariable Long protestId) {
-        CheerStat stats = cheerDbService.getCheerStat(protestId);
+        CheerStat stats = cheerCacheService.getCheerStat(protestId);
         log.debug("응원 통계 조회 - 시위 ID: {}, 응원 수: {}", protestId, stats.cheerCount());
         return ResponseEntity.ok(stats);
     }
@@ -41,7 +39,7 @@ public class CheerRestController {
      */
     @GetMapping
     public ResponseEntity<List<CheerStat>> getAllCheerStat() {
-        List<CheerStat> statsList = cheerDbService.getAllCheerStats();
+        List<CheerStat> statsList = cheerCacheService.getAllCheerStats();
         log.debug("모든 시위 응원 통계 조회 - 시위 개수: {}", statsList.size());
         return ResponseEntity.ok(statsList);
     }
@@ -54,7 +52,7 @@ public class CheerRestController {
      */
     @PostMapping("/{protestId}")
     public ResponseEntity<CheerStat> cheerProtest(@PathVariable Long protestId) {
-        Integer cheerCount = cheerDbService.cheerProtest(protestId);
+        Integer cheerCount = cheerCacheService.cheerProtest(protestId);
         log.debug("REST API 응원 요청 - 시위 ID: {}, 응원 후 카운트: {}", protestId, cheerCount);
 
         CheerStat stats = new CheerStat(protestId, cheerCount);
