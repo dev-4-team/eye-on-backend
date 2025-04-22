ffc1e1ef7958fc192ab0e569dc490b47547ffd98
smartcow <1995hyunwoo@naver.com>
2025-04-22 13:18:27 +0900

    feat(protest-error-code): add cheer count not found error code

--- src/main/java/com/on/eye/api/protest/error/exception/ProtestErrorCode.java
+++ src/main/java/com/on/eye/api/protest/error/exception/ProtestErrorCode.java
@@ -16,7 +16,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ProtestErrorCode implements BaseErrorCode {
     PROTEST_NOT_FOUND(NOT_FOUND, "PROTEST_400_1", "해당 id의 시위 정보를 조회할 수 없습니다"),
-    PROTEST_CACHE_NOT_FOUND(NOT_FOUND, "PROTEST_400_2", "Cache에서 해당 id의 시위 정보를 조회할 수 없습니다"),
+    PROTEST_CHEER_COUNT_NOT_FOUND(NOT_FOUND, "PROTEST_400_2", "해당 id의 시위 응원 정보를 조회할 수 없습니다"),
+    PROTEST_CACHE_NOT_FOUND(NOT_FOUND, "PROTEST_400_3", "Cache에서 해당 id의 시위 정보를 조회할 수 없습니다"),
     DUPLICATED_VERIFICATION(CONFLICT, "PROTEST_409_1", "한개의 시위에 중복 인증은 불가능합니다"),
     OUT_OF_VALID_PROTEST_RANGE(UNPROCESSABLE_ENTITY, "PROTEST_422_1", "유효한 시위 참여인증 범위 밖에 있습니다"),
     ABNORMAL_MOVEMENT_PATTERN(UNPROCESSABLE_ENTITY, "PROTEST_422_2", "불가능한 이동속도 인증입니다");
